01d55976c81b1a3bce8176e71363c9127370a640
FRNKCSTL <70352218+FRNKCSTL@users.noreply.github.com>
2020-08-29 00:38:07 -0600

    Create Geo.java

---
+++ src/com/grupo5/Country/Geo.java
@@ -0,0 +1,9 @@
+public class Geo extends Country{
+ 
+
+  public Geo{
+    System.out.println("Guatemala posee 22 Depatamentos")
+    System.out.println("Se encuentra en América Central")
+    System.out.println("")
+   }
+}
